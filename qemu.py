55123572f4c3b42761bda17cc62b49943e23d182
LucasHillebrand <lucashillebrand05@gmail.com>
2025-06-15 17:04:43 +0200

    updated qemu script

--- qemu.py
+++ qemu.py
@@ -31,6 +31,9 @@ while (i<len(osargs)):
         case "-net":
             args.append(f"-netdev user,id=id{idctr} -device e1000,netdev=id{idctr}")
             idctr+=1
+        case "-tor":
+            args.append(f"-netdev 'user,restrict=on,guestfwd=tcp:10.0.2.4:9050-cmd:nc 127.0.0.1 9050,guestfwd=tcp:10.0.2.4:53-cmd:nc -u 1.1.1.1 53,id=id{idctr}' -device e1000,netdev=id{idctr}")
+            idctr+=1
         case "-netfwd":
             i+=1
             ports=osargs[i].split(",")
@@ -61,11 +64,18 @@ while (i<len(osargs)):
                     -cd [iso name] // adds a virtual disk image to the machine\n\
                     -usb [image name] // adds a virtual usb stick to the vm\n\
                     -net // adds network to the machine\n\
+                    -tor // adds a network adapter which can only connect to the tor proxy running on the local system (on port 9050) and for dns services the port 53 to cloudflare dns\n\
                     -netfwd [{dst port}:{src port},{...}] // adds network with port forwarding\n\
                     -audio // adds audio\n\
                     -cores [corecount] // specifies the corecount of the machine, default 4\n\
                     -video [device name] // specifies the video driver, default virtio\n\
-                    -mem [size (.../G/M/K)] // specifies virtual memory, default 8G")
+                    -mem [size (.../G/M/K)] // specifies virtual memory, default 8G\n\
+                    \n\
+                    requirements for this script:\n\
+                    - tor\n\
+                    - netcat\n\
+                    - qemu\n\
+                    - python")
             run=False
     i+=1
 
